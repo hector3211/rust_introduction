17ed217b86ed5be97114286161a5475c7658a05c
hector3211 <hectororopesa5@gmail.com>
2022-08-15 19:54:52 -0400

    actix folder struct addition

---
+++ actix_rust_api/src/api/task.rs
@@ -0,0 +1,50 @@
+use actix_web::{get, post, put, web, HttpRequest, HttpResponse, Responder, Result};
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+#[derive(Serialize, Deserialize)]
+pub struct User {
+    name: String,
+}
+// add to database
+#[get("/json/{name}")]
+pub async fn json_data(name: web::Path<User>) -> web::Json<String> {
+    return web::Json(name.into_inner().name);
+}
+
+#[derive(Deserialize)]
+pub struct Info {
+    username: String,
+}
+
+// deserialize `Info` from request's body
+/*
+ * {
+ *  "username": "<input value>"
+ * }
+ * so its http://127.0.0.1:8080/username
+ */
+#[get("/")]
+pub async fn index(info: web::Json<Info>) -> Result<String> {
+    Ok(format!("Welcome {}!", info.username))
+}
+
+#[derive(Deserialize)]
+pub struct FormData {
+    user: String,
+}
+
+// port: 8080/hector
+#[post("/user")]
+pub async fn user(form: web::Form<FormData>) -> Result<String> {
+    Ok(format!("Welcome {}!", form.user))
+}
+
+// here we GET our "data"
+pub async fn data() -> impl Responder {
+    let mut map = HashMap::new();
+    map.insert("key1", "value1");
+    map.insert("key2", "value2");
+    map.insert("key3", "value3");
+    HttpResponse::Ok().json(map)
+}
--- actix_rust_api/src/main.rs
+++ actix_rust_api/src/main.rs
@@ -1,21 +1,7 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, Result};
-use serde::Deserialize;
-use std::collections::HashMap;
-#[derive(Deserialize)]
-struct Info {
-    username: String,
-}
-
-// deserialize `Info` from request's body
-/*
- * {
- *  "username": "<input value>"
- * }
- */
-#[get("/")]
-async fn index(info: web::Json<Info>) -> Result<String> {
-    Ok(format!("Welcome {}!", info.username))
-}
+use serde::{Deserialize, Serialize};
+mod api;
+use api::task::{data, index, json_data, user};
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -27,32 +13,16 @@ async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
 
-#[derive(Deserialize)]
-struct FormData {
-    user: String,
-}
-
-#[post("/user")]
-async fn user(form: web::Form<FormData>) -> Result<String> {
-    Ok(format!("Welcome {}!", form.user))
-}
-
 async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
-async fn data() -> impl Responder {
-    let mut map = HashMap::new();
-    map.insert("key1", "value1");
-    map.insert("key2", "value2");
-    map.insert("key3", "value3");
-    HttpResponse::Ok().json(map)
-}
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(index)
+            .service(json_data)
             .service(user)
             .service(hello)
             .service(echo)
